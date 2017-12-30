7257a22494dfb356a58764928a3a11314ccb5fe5
Damilare <dre4success@live.com>
2017-12-30 02:17:53 +0100

    a react native project for checking out places

---
+++ App.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import PlaceInput from './src/components/PlaceInput';
+import PlaceList from './src/components/PlaceList';
+import PlaceDetail from './src/components/PlaceDetail';
+import placeImage from './src/assets/beautiful-place.jpg';
+
+export default class App extends Component {
+  state = {
+    places: [],
+    selectedPlace: null
+  };
+
+  placeAddedHandler = placeName => {
+    this.setState(prevState => ({
+      places: [
+        ...prevState.places,
+        { key: Math.random(), name: placeName, image: placeImage }
+      ]
+    }));
+  };
+
+  placeSelected = key => {
+    this.setState(prevState => ({
+      selectedPlace: prevState.places.find(place => {
+        return place.key === key;
+      })
+    }));
+    /* 
+    */
+  };
+
+  placeDeletedHandler = () => {
+    this.setState(prevState => {
+      return {
+        places: prevState.places.filter(place => {
+          return place.key !== prevState.selectedPlace.key;
+        }),
+        selectedPlace: null
+      };
+    });
+  };
+
+  modalClosedHandler = () => {
+    this.setState(() => ({ selectedPlace: null }));
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <PlaceDetail
+          selectedPlace={this.state.selectedPlace}
+          onItemDeleted={this.placeDeletedHandler}
+          onModalClosed={this.modalClosedHandler}
+        />
+        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
+        <PlaceList
+          places={this.state.places}
+          onItemSelected={this.placeSelected}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 26,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  }
+});
