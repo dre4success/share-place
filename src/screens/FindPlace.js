2bcc4d4f7f1bcb4aae1f6ae0c06f33b454d1f321
Damilare <dre4success@live.com>
2018-01-08 16:14:53 +0100

    Making a sidedrawer navigation

--- src/screens/FindPlace.js
+++ src/screens/FindPlace.js
@@ -5,31 +5,50 @@ import { connect } from 'react-redux';
 import PlaceList from '../components/PlaceList';
 
 class FindPlaceScreen extends Component {
-  
+
+
+  constructor(props) {
+    super(props);
+    this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
+  }
+
+  onNavigatorEvent = event => {
+    if (event.type === 'NavBarButtonPress') {
+      if (event.id === 'sideDrawerToggle') {
+        this.props.navigator.toggleDrawer({
+          side: 'left'
+        });
+      }
+    }
+  };
+
   itemSelectedHandler = key => {
-    const selectPlace = this.props.places.find(place => (place.key === key));
+    const selectPlace = this.props.places.find(place => place.key === key);
     this.props.navigator.push({
       screen: 'awesome-places.PlaceDetailScreen',
       title: selectPlace.name,
       passProps: {
-        selectedPlace: selectPlace        
+        selectedPlace: selectPlace
       }
-    })
-  }
+    });
+  };
 
-  render() { 
-    return ( 
+  render() {
+    return (
       <View>
-        <PlaceList places={this.props.places} onItemSelected={this.itemSelectedHandler} />
+        <PlaceList
+          places={this.props.places}
+          onItemSelected={this.itemSelectedHandler}
+        />
       </View>
-     )
+    );
   }
 }
 
 const mapStateToProps = state => {
   return {
     places: state.places.places
-  }
-}
- 
+  };
+};
+
 export default connect(mapStateToProps)(FindPlaceScreen);
