aec0ed01b86289c91793efcb74beb245c9f245e2
Damilare <dre4success@live.com>
2018-01-08 16:13:49 +0100

    creating a reusable input component

---
+++ src/components/UI/DefaultInput.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { TextInput, StyleSheet } from 'react-native';
+
+export const DefaultInput = props => (
+  <TextInput
+    style={styles.input}
+    underlineColorAndroid="transparent"
+    {...props}
+  />
+);
+
+const styles = StyleSheet.create({
+  input: {
+    width: '100%',
+    borderWidth: 1,
+    borderColor: '#eee',
+    padding: 5,
+    margin: 8
+  }
+});
