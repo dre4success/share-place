422eb586f85c058cbfd038a860045bd96ae676e8
Damilare <dre4success@live.com>
2018-01-08 16:15:19 +0100

    creating a sideDrawer Nagivation

--- src/screens/startMainTabs.js
+++ src/screens/startMainTabs.js
@@ -2,9 +2,10 @@ import { Navigation } from 'react-native-navigation';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 export const startTabs = async () => {
-  const [find, share] = await Promise.all([
+  const [find, share, menu] = await Promise.all([
     Icon.getImageSource('md-map', 30),
-    Icon.getImageSource('md-share', 30)
+    Icon.getImageSource('md-share', 30),
+    Icon.getImageSource('md-menu', 30)
   ]);
 
   Navigation.startTabBasedApp({
@@ -13,14 +14,37 @@ export const startTabs = async () => {
         screen: 'awesome-places.SharePlaceScreen',
         label: 'Share Place',
         title: 'Share Place',
-        icon: share
+        icon: share,
+        navigatorButtons: {
+          leftButtons: [
+            {
+              icon: menu,
+              title: 'Menu',
+              id: 'sideDrawerToggle'
+            }
+          ]
+        }
       },
       {
         screen: 'awesome-places.FindPlaceScreen',
         label: 'Find Place',
         title: 'Find Place',
-        icon: find
+        icon: find,
+        navigatorButtons: {
+          leftButtons: [
+            {
+              icon: menu,
+              title: 'Menu',
+              id: 'sideDrawerToggle'
+            }
+          ]
+        }
       }
-    ]
+    ],
+    drawer: {
+      left: {
+        screen: 'awesome-places.SideDrawerScreen'
+      }
+    }
   });
 };
