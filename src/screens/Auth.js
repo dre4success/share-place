43afb93bab26f3c8e5d145a31a5c12bb65611248
Damilare <dre4success@live.com>
2018-01-09 21:38:38 +0100

    disabling submit button if invalid

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -26,21 +26,24 @@ class AuthScreen extends Component {
         valid: false,
         validationRules: {
           isEmail: true
-        }
+        },
+        touched: false
       },
       password: {
         value: '',
         valid: false,
         validationRules: {
           minLength: 6
-        }
+        },
+        touched: false
       },
       confirmPassword: {
         value: '',
         valid: false,
         validationRules: {
           equalTo: 'password'
-        }
+        },
+        touched: false
       }
     }
   };
@@ -101,7 +104,8 @@ class AuthScreen extends Component {
             value,
             prevState.controls[key].validationRules,
             connectedValue
-          )
+          ),
+          touched: true
         }
       }
     }));
@@ -130,6 +134,8 @@ class AuthScreen extends Component {
               style={styles.input}
               value={this.state.controls.email.value}
               onChangeText={val => this.updateInputStates('email', val)}
+              valid={this.state.controls.email.valid}
+              touched={this.state.controls.email.touched}
             />
             <View
               style={
@@ -150,6 +156,8 @@ class AuthScreen extends Component {
                   style={styles.input}
                   value={this.state.controls.password.value}
                   onChangeText={val => this.updateInputStates('password', val)}
+                  valid={this.state.controls.password.valid}
+                  touched={this.state.controls.password.touched}
                 />
               </View>
               <View
@@ -166,11 +174,21 @@ class AuthScreen extends Component {
                   onChangeText={val => {
                     this.updateInputStates('confirmPassword', val);
                   }}
+                  valid={this.state.controls.confirmPassword.valid}
+                  touched={this.state.controls.confirmPassword.touched}
                 />
               </View>
             </View>
           </View>
-          <ButtonWithBackground color="#29aaf4" onPress={this.loginHandler}>
+          <ButtonWithBackground
+            color="#29aaf4"
+            onPress={this.loginHandler}
+            disabled={
+              !this.state.controls.confirmPassword.valid ||
+              !this.state.controls.email.valid ||
+              !this.state.controls.password.valid
+            }
+          >
             Submit
           </ButtonWithBackground>
         </View>
