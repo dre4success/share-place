ca118fe9b09de851c3b5a60a85f5ca236f64fd44
Damilare <dre4success@live.com>
2018-01-11 21:46:59 +0100

    creating a feedback action for the user

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,39 +1,32 @@
 import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
 export const addPlace = (placeName, location, image) => dispatch => {
-  const placeData = {
-    name: placeName,
-    location
-  };
-
-     fetch(
+  fetch(
     'https://us-central1-places-awesome-1515590111232.cloudfunctions.net/storeImage',
     {
       method: 'POST',
       body: JSON.stringify({
         image: image.base64
       })
-    
+    }
   )
     .catch(err => console.log(err))
     .then(res => res.json())
     .then(parsedRes => {
-      console.log(parsedRes);
-    }); 
-
-  /* fetch('https://places-awesome-1515590111232.firebaseio.com/places.json', {
-    method: 'POST',
-    body: JSON.stringify(placeData)
-  })
+      const placeData = {
+        name: placeName,
+        location,
+        image: parsedRes.imageUrl
+      };
+      return fetch(
+        'https://places-awesome-1515590111232.firebaseio.com/places.json',
+        {
+          method: 'POST',
+          body: JSON.stringify(placeData)
+        }
+      );
+    })
     .catch(err => console.log(err))
-    .then(res => res.json())
-    .then(parsedRes => {
-      console.log(parsedRes);
-    }); */
-  /*    const res = await fetch('https://places-awesome-1515590111232.firebaseio.com/places.json', {
-      method: 'POST',
-      body: JSON.stringify(placeData)
-    }) */
 };
 
 export const deletePlace = key => {
